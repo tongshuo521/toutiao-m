a0dbe7dad3402161fe7af68c252ca41c09eeeecc
xtt-it <17634415530@163.com>
2022-09-05 19:40:37 +0800

    项目初始化-导入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 引入vant组件库
+import Vant from "vant";
+import "vant/lib/index.css";
+
 // 引入全局样式
 import "@/styles/index.less";
+
+// 组册组件库
+Vue.use(Vant);
+
 Vue.config.productionTip = false;
 
 new Vue({
